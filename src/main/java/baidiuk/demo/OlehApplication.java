8104348be0cbc1780ef24d54d1e1d32950fd451b
olehb92 <olehb92@unet.univie.ac.at>
2020-07-28 16:25:06 +0200

    init commit

---
+++ src/main/java/baidiuk/demo/OlehApplication.java
@@ -0,0 +1,13 @@
+package baidiuk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlehApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlehApplication.class, args);
+    }
+
+}
---
+++ src/test/java/baidiuk/demo/OlehApplicationTests.java
@@ -0,0 +1,13 @@
+package baidiuk.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlehApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
